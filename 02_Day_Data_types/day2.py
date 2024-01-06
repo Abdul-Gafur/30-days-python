0180ae5de814bbdfc0baf1a7ed64a3b28111d7f0
Abdul-Gafur <abdulgafurshaattir@gmail.com>
2024-01-06 22:03:59 +0000

    python datatypes

---
+++ 02_Day_Data_types/day2.py
@@ -0,0 +1,25 @@
+# Day 2 - Python data types
+
+
+# Text type - str
+# Numeric types - int, float, complex
+# Sequence types - list, tuple, range
+# Mapping Type
+# Set type
+# Boolean Type: bool
+# Binary Types: bytes, bytearray, memoryview
+# None Types: NoneType
+
+# Lets start by defining some data types
+
+name = "Ali" # String datatype
+x = 20       # Int datatype
+x = 20.5     # Float datatype
+x = 20j      # Complex datatype
+fruits = ["apple", "bannana", "cherry"]  # List datatype
+fruits = ("apple", "bannana", "cherry")  # Tuples datatype
+x = range(6) # Range datatype
+x = {"name": "John", "age": 30} # Dictionary datatype
+x = True # Boolean datatype
+
+print(x) 
